249d9cbcb8239a3d26a2082ba55a5934a52bc7e0
vlad <vladislavcarik2014@gmail.com>
2023-07-09 14:30:44 +0300

    parse eth blocks

--- src/wallet/tasks.py
+++ src/wallet/tasks.py
@@ -1,12 +1,31 @@
+import asyncio
+
 from celery import shared_task
 from loguru import logger
-from web3 import Web3
 
-from config.config import settings
+from config.database import async_session
+from config.web3 import get_web3
+from src.wallet.service import get_all_wallets_address
 
 
-@shared_task
-def create_task(name: str):
-    w3 = Web3(Web3.HTTPProvider(settings.QUICK_NODE_URL))
-    logger.info(w3.eth.get_transaction_by_block("latest", 0))
-    return name
+async def get_addresses() -> list:
+    async with async_session() as session:
+        addresses = await get_all_wallets_address(session)
+    return addresses
+
+
+@shared_task(bind=True)
+def parse_eth_blocks(self, reply):
+    if reply:
+        parse_eth_blocks.apply_async(countdown=12, kwargs={"reply": True})
+    loop = asyncio.get_event_loop()
+    addresses = loop.run_until_complete(get_addresses())
+    logger.info(addresses)
+    w3 = get_web3()
+    block = w3.eth.get_block("latest", True)
+    logger.info(block.get("number"))
+    for transaction in block.get("transactions"):
+        if transaction.get("to") in addresses or transaction.get("from") in addresses:
+            logger.info(transaction)
+            logger.info(transaction["s"].hex())
+    return True
